a84d0a35b9d026ed3e14fabc06e42442f58da8f2
nikhil <nikhilesh1729@gmail.com>
2026-01-09 18:21:33 +0530

    upload project first time

---
+++ app/src/main/java/com/sagarsweets/in/MainActivity.java
@@ -0,0 +1,45 @@
+package com.sagarsweets.in;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Integer counter = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Button btnCounter = findViewById(R.id.btnCounter);
+        Button btnReset =  findViewById(R.id.btnReset);
+        TextView tvCounter = findViewById(R.id.txtCounter);
+        btnCounter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter++;
+                tvCounter.setText(" " + counter);
+            }
+        });
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter = 0;
+                tvCounter.setText(" "+ counter);
+            }
+        });
+    }
+
+}
