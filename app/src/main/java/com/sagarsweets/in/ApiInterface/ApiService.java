28c98c5d086202a87431fa317af7896ca3796aaf
nikhil <nikhilesh1729@gmail.com>
2026-01-17 15:57:31 +0530

    home fragment slider with shimmer

--- app/src/main/java/com/sagarsweets/in/ApiInterface/ApiService.java
+++ app/src/main/java/com/sagarsweets/in/ApiInterface/ApiService.java
@@ -8,11 +8,13 @@ import com.sagarsweets.in.ApiModel.LoginRequest;
 import com.sagarsweets.in.ApiModel.LoginResponse;
 import com.sagarsweets.in.ApiModel.OtpResponse;
 import com.sagarsweets.in.ApiModel.RegisterUserRequest;
+import com.sagarsweets.in.ApiModel.SliderResponse;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 public interface ApiService {
     @POST("rest/user/login")
@@ -40,4 +42,7 @@ public interface ApiService {
 
     @POST("rest/user/forgetpassword")
     Call<OtpResponse> forgetPassword(@Body ForgetPasswordRequest forgetPasswordRequest);
+
+    @GET("rest/homefragment/index")
+    Call<SliderResponse> getSliderImages();
 }
